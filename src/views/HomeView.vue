88378fe5664c6fb66852cf6e1154f49fb9a2b08b
Sebastian <sebastianlakla@gmail.com>
2026-06-06 11:09:26 +0200

    Migrated component site with 404 to components directiory and imported as async load from vue component to lazy load. Added async load for each section which is below header and above footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,49 @@
 <script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
 import HeaderMain from '@/components/Header/HeaderMain.vue'
-import FeaturesMain from '@/components/FeaturesSite/FeaturesMain.vue'
-import BrowserExtentions from '@/components/BrowserExtentions/ExtentionsMain.vue'
-import AccordionMain from '@/components/Accordion/AccordionMain.vue'
-import NewsletterMain from '@/components/Newsletter/NewsletterMain.vue'
+const FeaturesMain = defineAsyncComponent(
+  () => import('@/components/FeaturesSite/FeaturesMain.vue'),
+)
+const BrowserExtentions = defineAsyncComponent(
+  () => import('@/components/BrowserExtentions/ExtentionsMain.vue'),
+)
+const AccordionMain = defineAsyncComponent(() => import('@/components/Accordion/AccordionMain.vue'))
+const NewsletterMain = defineAsyncComponent(
+  () => import('@/components/Newsletter/NewsletterMain.vue'),
+)
 </script>
 
 <template>
-    <HeaderMain />
-    <FeaturesMain />
-    <BrowserExtentions />
-    <AccordionMain />
-    <NewsletterMain />
+  <HeaderMain />
+  <Suspense>
+    <template #default>
+      <div class="site-wrapper">
+        <FeaturesMain />
+        <BrowserExtentions />
+        <AccordionMain />
+        <NewsletterMain />
+      </div>
+    </template>
+
+    <template #fallback>
+      <div class="sections-loader">
+        <p>Loding...</p>
+      </div>
+    </template>
+  </Suspense>
 </template>
+<style scoped lang="scss">
+@use 'sass:map';
+
+@use '@/assets/sass/mixins.scss' as *;
+.site-wrapper{
+@include flex-layout($flex-direction: column);
+@include set-gap(6em, 0)
+}
+.sections-loader {
+  padding: 5em;
+  text-align: center;
+  font-weight: 500;
+  opacity: 0.7;
+}
+</style>
--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -1,53 +1,22 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+const ErrorSite = defineAsyncComponent(() => import('@/components/ErrorSite/Site404.vue'))
+</script>
 <template>
-  <div class="not-found-page">
-    <h1 class="not-found-page__header">Error 404</h1>
-    <p class="not-found-page__description">Site has not be found.</p>
-    <router-link to="/" class="not-found-page__button">Back to main site</router-link>
-  </div>
+  <Suspense>
+    <template #default>
+        <ErrorSite />
+    </template>
+    <template #fallback>
+      <div class="loader-placeholder"></div>
+    </template>
+  </Suspense>
 </template>
 <style lang="scss" scoped>
-@use 'sass:map';
-@use '@/assets/sass/colors.scss' as *;
-@use '@/assets/sass/breakpoints.scss' as *;
-@use '@/assets/sass/mixins.scss' as *;
-@media (min-width: $mobile-view) {
-  .not-found-page {
-    @include flex-layout($flex-direction: column, $justify-content: center, $align-items: center);
-    @include set-gap(1em, 0);
-    padding-top: 6em;
-    height: 60svh;
-    &__header {
-      color: map.get($colors, 'neutral-blue-950');
-    }
-    &__description {
-      color: map.get($colors, 'neutral-grey-600');
-    }
-    &__button {
-      color: map.get($colors, 'neutral-grey-50');
-      background-color: map.get($colors, 'primary-blue-600');
-      border: 0.15em solid map.get($colors, 'neutral-grey-50');
-      padding: 0.75em 1em;
-      border-radius: 0.4em;
-      align-self: center;
-      transition:
-        color 0.3s ease-in-out,
-        border-color 0.3s ease-in-out,
-        background-color 0.3s ease-in-out;
-      &:hover {
-        background-color: map.get($colors, 'neutral-grey-50');
-        color: map.get($colors, 'primary-blue-600');
-        border-color: map.get($colors, 'primary-blue-600');
-      }
-    }
-  }
-}
-@media (min-width: $desktop-small){
-  .not-found-page{
-    padding-top: 0;
-   padding-bottom: 6em;
-    height: 50svh;
-  }
+.loader-placeholder {
+  padding: 5em;
+  text-align: center;
+  font-weight: 500;
+  opacity: 0.7;
 }
 </style>
